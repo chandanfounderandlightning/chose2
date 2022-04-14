10ad06b0547ee20ae1d1aff4c071165ad35985ce
Chandan <chandankumar@ucreate.co.in>
2022-04-14 12:21:32 +0530

    basic

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="welcome-text">
+          Welcome to chose 2
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
